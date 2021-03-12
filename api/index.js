561d9220bbd6672bf17314717352d2cbd3d74851
broozkan <burhan.ozkan@live.com>
2021-03-12 08:50:27 +0300

    Updated cors policy

--- api/index.js
+++ api/index.js
@@ -30,7 +30,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // cors middleware
-app.use(cors());
+var allowedOrigins = [
+  'http://localhost:3000',
+  'https://turkeyballoonscappadocia.com',
+  'https://www.turkeyballoonscappadocia.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 app.use(express.static('/public'));
 
